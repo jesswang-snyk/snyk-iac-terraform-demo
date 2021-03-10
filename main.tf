9ef5d942b8af0a3d3f2a1eece4348f2b148be864
Vishal Soni <vsonisnyk@Vishals-MacBook-Pro.local>
2021-03-10 13:47:54 -0800

    Changed the file for Demo

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ provider "aws" {
 
 resource "aws_iam_account_password_policy" "strict" {
   minimum_password_length = 8
-  #require_lowercase_characters   = true
+  require_lowercase_characters   = true
   #require_numbers                = true
   #require_uppercase_characters   = true
   #require_symbols                = true
